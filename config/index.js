1b42eec02ab228ed8e2f85c222502eabafc611c9
Mohan <mohankarthikeyan@Mohans-MacBook-Pro.local>
2021-11-25 23:02:00 +0530

    server creation

---
+++ config/index.js
@@ -0,0 +1,5 @@
+const config = {
+    DATABASE_URI: "mongodb+srv://Vijayakumar:gzH6QzbUcYEjRKcj@cluster0.ise85.mongodb.net/dms?retryWrites=true&w=majority"
+};
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,2 +1,31 @@
 const grpc = require("grpc");
-const protoLoader = require("@grpc/proto-loader")
+const protoLoader = require("@grpc/proto-loader");
+require('./lib/mongo')
+
+console.log("Before Loading")
+const packageDefinition = protoLoader.loadSync("dms.proto", {});
+// const packageDefinition = protoLoader.loadSync('./dms.proto', {
+//     keepCase: true,
+//     longs: String,
+//     enums: String,
+//     defaults: true,
+//     oneofs: true
+// });
+
+const dmsPackageDefinition = grpc.loadPackageDefinition(packageDefinition).dmsPackage;
+//console.log("DMS DEF",dmsPackageDefinition)
+
+const Services = require('./services/userServices')
+
+const server = new grpc.Server();
+server.addService(dmsPackageDefinition.DocumentManagement.service, {
+    register: Services.register,
+    login: Services.login,
+    // getSingleTodo: Controller.Todo.getSingleTodo,
+    // deleteTodo: Controller.Todo.deleteTodo,
+    // updateTodo: Controller.Todo.updateTodo
+});
+
+server.bind('localhost:50051', grpc.ServerCredentials.createInsecure());
+console.log('Server running at localhost:50051');
+server.start();
